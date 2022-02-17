6daed992658650d64610670b136fe9ed1285216f
Simple <varg616@gmail.com>
2022-02-17 12:21:12 +0300

    some

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-import Success from "./Success";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Success from "./Success";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
